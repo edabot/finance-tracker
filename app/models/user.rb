ffdc982b9dfe051eeed5f9931219e32756323857
edabot <ed.lewis@gmail.com>
2016-02-08 05:47:50 +0000

    block user from adding 10+ or existing stocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ActiveRecord::Base
          
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+  
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+  
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol) #find stock in stock table
+    return false unless stock  #return false if already added to stock table
+    user_stocks.where(stock_id: stock.id).exists?  #see if user already has stock in useR_stock table
+  end
+
 end
